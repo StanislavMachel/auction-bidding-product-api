4c9bf1709993f93b8cff540f9b0b396803316ebb
stanislav.machel <stanislav.machel@gmail.com>
2018-12-18 14:05:58 +0200

    Create Auction Dto

---
+++ AuctionBiddingProduct.WebApi/Dto/AuctionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionBiddingProduct.WebApi.Dto
+{
+    public class AuctionDto
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCategory { get; set; }
+        public DateTime BiddingEndDate { get; set; }
+    }
+}
