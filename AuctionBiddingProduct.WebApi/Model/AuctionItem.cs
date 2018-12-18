82961fcd1ef0693e34335370c56140298d0ed730
stanislav.machel <stanislav.machel@gmail.com>
2018-12-18 13:55:42 +0200

    Create AuctionItem entity

---
+++ AuctionBiddingProduct.WebApi/Model/AuctionItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionBiddingProduct.WebApi.Model
+{
+    public class AuctionItem
+    {
+        public Guid Id { get; set; }
+        public Product Product { get; set; }
+        public DateTime BiddingEndDate { get; set; }
+    }
+}
